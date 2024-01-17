9b9f61f137dadf6cde3ab03033d762f4e05d5bdb
nzi19001@byui.edu <nzi19001@byui.edu>
2024-01-17 16:41:27 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('The Dom has Loaded')
+});
+console("This console.log() fires when index.js loads - before DOMContentLoaded is triggered")
+
